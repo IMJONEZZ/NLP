23d23c512e201761b5c82f171cfdeea219dc55e3
IMJONEZZ <chrisbrousseau304@gmail.com>
2021-01-05 21:04:07 -0800

    Deleted Google News dataset (Text document to remember which dataset)

--- NLP with Pytorch/tensors.py
+++ NLP with Pytorch/tensors.py
@@ -90,3 +90,37 @@ y = torch.rand(3, 3)
 cpu_device = torch.device("cpu")
 y = y.to(cpu_device)
 x = x.to(cpu_device)
+
+
+# Exercises
+#1.Create a 2D tensor and then add a dimension of size 1 inserted at dimension 0.
+t = torch.rand(3, 3)
+describe(t)
+t = t.unsqueeze(0)
+describe(t)
+# 2.Remove the extra dimension you just added to the previous tensor.
+t = t.squeeze(0)
+describe(t)
+# 3.Create a random tensor of shape 5x3 in the interval [3, 7)
+t = 3 + torch.rand(5, 3) * (7 - 3)
+describe(t)
+# 4.Create a tensor with values from a normal distribution (mean=0, std=1).
+t = torch.rand(3, 3)
+t.normal_()
+describe(t)
+# 5.Retrieve the indexes of all the nonzero elements in the tensor torch.Tensor([1,1, 1, 0, 1]).
+t = torch.Tensor([1,1,1,0,1])
+a = torch.nonzero(t)
+describe(a)
+# 6.Create  a  random  tensor  of  size (3,1)  and  then  horizontally  stack  four  copiestogether.
+t = torch.rand(3,1)
+t = t.expand(3,4)
+describe(t)
+# 7.Return  the  batch  matrix-matrix  product  of  two  three-dimensional  matrices(a=torch.rand(3,4,5), b=torch.rand(3,5,4)).
+a = torch.rand(3,4,5)
+b=torch.rand(3,5,4)
+describe(torch.bmm(a,b))
+# 8.Return  the  batch  matrix-matrix  product  of  a  3D  matrix  and  a  2D  matrix(a=torch.rand(3,4,5), b=torch.rand(5,4)).
+a = torch.rand(3,4,5)
+b = torch.rand(5,4)
+describe(torch.bmm(a, b.unsqueeze(0).expand(a.size(0), *b.size())))
