0c31127b2c6a2e60829ec572e671192bd1cfff84
IMJONEZZ <chrisbrousseau304@gmail.com>
2022-09-28 00:19:59 -0600

    GPTKant added

--- Transformers/GPT-J/GPTJ.py
+++ Transformers/GPT-J/GPTJ.py
@@ -2,7 +2,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
 model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", revision="float16",
-                                torch_dtype=torch.float16, low_cpu_mem_usage=True)
+                                torch_dtype=torch.float16, low_cpu_mem_usage=False)
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
 CONTEXT = str(input("Enter the context: "))
 
